7ff41db20d37df11447e14a5d0c6d9779f16aaa8
p0p4k <rajiv.punmiya@gmail.com>
2023-08-22 01:30:54 +0900

    typo

--- train.py
+++ train.py
@@ -158,7 +158,7 @@ def run(rank, n_gpus, hps):
       eps=hps.train.eps)
   if net_dur_disc is not None:
     optim_dur_disc = torch.optim.AdamW(
-        net_dur_disc.parametrs(),
+        net_dur_disc.parameters(),
         hps.train.learning_rate,
         betas=hps.train.betas,
         eps=hps.train.eps)
