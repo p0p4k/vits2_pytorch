2f09eae8aa914b1479e8790fe8f4e1c135a3b9d7
Ilya Shigabeev <beat@live.ru>
2023-08-30 00:34:37 +0300

    feat: output_dir in argparser

--- utils.py
+++ utils.py
@@ -147,9 +147,11 @@ def get_hparams(init=True):
                         help='JSON file for configuration')
     parser.add_argument('-m', '--model', type=str, required=True,
                         help='Model name')
+    parser.add_argument('-d', '--dir', type=str, default="./logs",
+                        help='Directory for checkpoints')
 
     args = parser.parse_args()
-    model_dir = os.path.join("./logs", args.model)
+    model_dir = os.path.join(args.dir, args.model)
 
     if not os.path.exists(model_dir):
         os.makedirs(model_dir)
